0bf5c9bb2a79718c38cadd3af0b34315993115ed
Michael Petersen <michael@smoerfugl.dk>
2024-02-19 19:24:31 +0100

    remake repo from VS

---
+++ Michael/ASP-MVC-Exercises/ASP-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
